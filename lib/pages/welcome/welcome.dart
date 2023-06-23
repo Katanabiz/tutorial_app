540236b4495f9bddcf140554a1cfa20ac7a5a697
Tokzhan Beisekenova <tokzhan@longevityintime.org>
2023-06-23 17:11:25 +0800

    dots indicator done

--- lib/pages/welcome/welcome.dart
+++ lib/pages/welcome/welcome.dart
@@ -1,6 +1,11 @@
 import 'package:dots_indicator/dots_indicator.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:tutorial_app/pages/welcome/bloc/welcome_blocs.dart';
+import 'package:tutorial_app/pages/welcome/bloc/welcome_state.dart';
+
+import 'bloc/welcome_events.dart';
 
 class Welcome extends StatefulWidget {
   const Welcome({super.key});
@@ -15,13 +20,20 @@ class _WelcomeState extends State<Welcome> {
     return Container(
       color: Colors.white,
       child: Scaffold(
-        body: Container(
+        body: BlocBuilder<WelcomeBloc,WelcomeState>(builder: (context,state){
+          return  Container(
           margin: EdgeInsets.only(top: 34.h),
           width: 375.w,
           child: Stack(
             alignment: Alignment.topCenter,
             children: [
-              PageView(children: [
+              PageView(
+                onPageChanged:(index){
+                  state.page = index;
+                  BlocProvider.of<WelcomeBloc>(context).add(WelcomeEvent());
+                  print("index value is ${index}");
+                },
+                children: [
                 _welcomePage(
                     1,
                     context,
@@ -47,18 +59,20 @@ class _WelcomeState extends State<Welcome> {
               Positioned(
                   bottom: 100.h,
                   child: DotsIndicator(
+                    position: state.page,
                       dotsCount: 3,
                       mainAxisAlignment: MainAxisAlignment.center,
                       decorator: DotsDecorator(
                           color: Colors.grey,
                           activeColor: Colors.blue,
                           size: const Size.square(8.0),
-                          activeSize: const Size(10.0, 8.0),
+                          activeSize: const Size(18.0, 8.0),
                           activeShape: RoundedRectangleBorder(
                               borderRadius: BorderRadius.circular(5.0)))))
             ],
           ),
-        ),
+        );
+        })
       ),
     );
   }
